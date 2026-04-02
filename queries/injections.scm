8734a83553efa649828bee85103876cff7714fe1
David Scotson <david.scotson@gmail.com>
2026-04-02 11:37:35 +0100

    add html and json injection

--- queries/injections.scm
+++ queries/injections.scm
@@ -1,2 +1,19 @@
 ((comment_statement) @injection.content
-  (#set! injection.language "comment"))
+ (#match? @injection.content "json")
+ (#set! injection.language "json"))
+
+((comment_statement) @injection.content
+ (#not-match? @injection.content "json")
+ (#set! injection.language "comment"))
+
+(section
+  (section_begin
+    (tag_name) @tag_name
+    (#eq? @tag_name "js"))
+  (text) @injection.content
+  (#set! injection.language "javascript")
+  (#set! injection.combined))
+
+((text) @injection.content
+ (#set! injection.language "html")
+ (#set! injection.combined))
